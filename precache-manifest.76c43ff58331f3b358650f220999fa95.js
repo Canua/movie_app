260e36068a1742403ae43f4e4d1cfb15a2fd59ab
canua <choigisuk2@naver.com>
2019-04-14 23:10:31 +0900

    Updates

---
+++ precache-manifest.76c43ff58331f3b358650f220999fa95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  },
+  {
+    "revision": "154924f324d807694d72",
+    "url": "/movie_app/static/js/main.edf00bde.chunk.js"
+  },
+  {
+    "revision": "74286a96ace76faac2ea",
+    "url": "/movie_app/static/js/2.5425dfe7.chunk.js"
+  },
+  {
+    "revision": "154924f324d807694d72",
+    "url": "/movie_app/static/css/main.aedb9283.chunk.css"
+  },
+  {
+    "revision": "3cf57c4df4f3b75f77eb22ed459127f0",
+    "url": "/movie_app/index.html"
+  }
+];
